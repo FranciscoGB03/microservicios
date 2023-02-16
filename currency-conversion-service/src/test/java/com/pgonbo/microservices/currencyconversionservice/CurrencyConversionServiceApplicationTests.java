cebdf389a7ebcc7881220e7c61c6cad1603d3937
FranciscoGB03 <fgonzalezb@toluca.tecnm.mx>
2023-02-15 23:03:08 -0600

    currency-conversion-service

---
+++ currency-conversion-service/src/test/java/com/pgonbo/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgonbo.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
