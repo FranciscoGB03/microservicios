a68968e8a511635716ecc8348cbb7356487cec79
FranciscoGB03 <fgonzalezb@toluca.tecnm.mx>
2023-03-16 23:03:21 -0600

    zipkin y microservio de current exchange service

--- currency-exchange-service/src/main/java/com/pgonbo/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/pgonbo/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,16 +1,16 @@
 package com.pgonbo.microservices.currencyexchangeservice.controller;
 
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.client.RestTemplate;
 
 import ch.qos.logback.classic.Logger;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+//import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 //import io.github.resilience4j.retry.annotation.Retry;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+//import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
--- currency-exchange-service/src/main/java/com/pgonbo/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/pgonbo/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.pgonbo.microservices.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,8 @@ import com.pgonbo.microservices.currencyexchangeservice.repository.CurrencyExcha
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     CurrencyExchangeRepository repository;
 
@@ -21,6 +25,9 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
             @PathVariable String to) {
+        // INFO [currency-exchange,d352c4a98affa7c1f0f38d43ef7d2d17,07e06035a4d73c89]
+        // 17872 --- [nio-8000-exec-4]
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
             throw new RuntimeException("Unable to Find data for " + from + " to " + to);
